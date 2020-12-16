27995fafd19bf4d41c4b9249a216b8baa2f5010b
Mr-wang-1 <62498033+Mr-wang-1@users.noreply.github.com>
2020-12-16 21:56:28 +0800

    Add files via upload
    滑动窗口

---
+++ 滑动窗口/Leetcode340.py
@@ -0,0 +1,35 @@
+"""
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: T is "ece" which its length is 3.
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: T is "aa" which its length is 2.
+
+"""
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        from collections import defaultdict
+        lookup = defaultdict(int)
+        start = 0
+        end = 0
+        max_len = 0
+        counter = 0
+        while end < len(s):
+            if lookup[s[end]] == 0:
+                counter += 1
+            lookup[s[end]] += 1
+            end += 1
+            while counter > k:
+                if lookup[s[start]] == 1:
+                    counter -= 1
+                lookup[s[start]] -= 1
+                start += 1
+            max_len = max(max_len, end - start)
+        return max_len
