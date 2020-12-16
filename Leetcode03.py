b9908ea01b7e9a482e7114d88e9d91818f131aa4
Mr-wang-1 <62498033+Mr-wang-1@users.noreply.github.com>
2020-12-16 21:57:10 +0800

    Add files via upload
    再提交一次

---
+++ Leetcode03.py
@@ -0,0 +1,96 @@
+"""
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+ 
+
+示例 1:
+
+输入: s = "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+示例 4:
+
+输入: s = ""
+输出: 0
+ 
+
+提示：
+
+0 <= s.length <= 5 * 104
+s 由英文字母、数字、符号和空格组成
+
+
+"""
+
+# 滑动窗口
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len:max_len = cur_len
+            lookup.add(s[i])
+        return max_len
+
+
+#循环更新
+class SolutionTwo:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = ''
+        tep = ''
+        for i in s:
+            if i not in tep:
+                tep += i
+            else:
+                tep = tep[tep.index(i)+1:]
+                tep +=i
+            if len(tep)>len(ans):
+                ans = tep
+            return len(ans)
+
+
+class SolutionThree:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        from collections import defaultdict
+        lookup = defaultdict(int)
+        start = 0
+        end = 0
+        max_len = 0
+        counter = 0
+        while end < len(s):
+            if lookup[s[end]] > 0:
+                counter += 1
+            lookup[s[end]] += 1
+            end += 1
+            while counter > 0:
+                if lookup[s[start]] > 1:
+                    counter -= 1
+                lookup[s[start]] -= 1
+                start += 1
+            max_len = max(max_len, end - start)
+        return max_len
+
